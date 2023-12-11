1528309acb1702f74c3d68967d3e46eb0342cc2d
Mr. Castelli <gianni_castelli@outlook.it>
2023-12-11 19:11:54 +0100

    Add project

---
+++ BaseObject.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Paratrooper2049
+{
+    internal class BaseObject
+    {
+        public enum gameObject
+        {
+            Bullet, Laser, Cluster, Heli, Para, Airplane, Gift
+        }
+
+        protected PointCollection _vertices = new PointCollection();
+        protected PointCollection _battleField = new PointCollection();
+
+        protected double _direction, _speed;
+        protected bool _lastFrame = false;
+        protected gameObject _description;
+
+        public PointCollection Vertices { get => _vertices; }
+
+        public double Speed { get => _speed; set => _speed = value; }
+
+        public bool LastFrame { get => _lastFrame; set => _lastFrame = value; }
+
+        public gameObject Description { get => _description; }
+
+        protected static Point RotateObject(Point point, Point center, double angle)
+        {
+            Point temp = new Point(point.X - center.X, point.Y - center.Y);
+
+            // Rotate the point by the given angle
+            double newX = temp.X * Math.Cos(angle) - temp.Y * Math.Sin(angle);
+            double newY = temp.X * Math.Sin(angle) + temp.Y * Math.Cos(angle);
+
+            // Translate the point back to its original position
+            newX += center.X;
+            newY += center.Y;
+
+            return new Point(newX, newY);
+        }
+
+        public static bool IntersectObject(PointCollection verticesA, PointCollection verticesB)
+        {
+            for (int i = 0; i < verticesA.Count; i++)
+            {
+                Point va = verticesA[i];
+                Point vb = verticesA[(i + 1) % verticesA.Count];
+
+                Point axis = new Point(va.Y - vb.Y, vb.X - va.X);
+
+                axis = Normalize(axis);
+
+                ProjectVertices(verticesA, axis, out double minA, out double maxA);
+                ProjectVertices(verticesB, axis, out double minB, out double maxB);
+
+                if (minA >= maxB || minB >= maxA)
+                {
+                    return false;
+                }
+            }
+
+            for (int i = 0; i < verticesB.Count; i++)
+            {
+                Point va = verticesB[i];
+                Point vb = verticesB[(i + 1) % verticesB.Count];
+
+                Point axis = new Point(va.Y - vb.Y, vb.X - va.X);
+
+                axis = Normalize(axis);
+
+                ProjectVertices(verticesA, axis, out double minA, out double maxA);
+                ProjectVertices(verticesB, axis, out double minB, out double maxB);
+
+                if (minA >= maxB || minB >= maxA)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private static void ProjectVertices(PointCollection vertices, Point axis, out double min, out double max)
+        {
+            min = double.MaxValue;
+            max = double.MinValue;
+
+            for (int i = 0; i < vertices.Count; i++)
+            {
+                Point p = vertices[i];
+                double proj = DotProduct(p, axis);
+
+                if (proj < min) { min = proj; }
+                if (proj > max) { max = proj; }
+            }
+        }
+
+        private static double DotProduct(Point a, Point b)
+        {
+            // a · b = ax * bx + ay * by
+            return a.X * b.X + a.Y * b.Y;
+        }
+
+        private static double Magnitude(Point p)
+        {
+            return Math.Sqrt(p.X * p.X + p.Y * p.Y);
+        }
+
+        private static Point Normalize(Point p)
+        {
+            double mag = Magnitude(p);
+            return new Point(p.X / mag, p.Y / mag);
+        }
+    }
+}
---
+++ Target.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Paratrooper2049
+{
+    internal class Target : BaseObject
+    {
+        private double heliWidth = 60, heliHeight = 30;
+        private double paraWidth = 10, paraHeight = 20;
+
+        private PointCollection _ground = new PointCollection();
+        private PointCollection _cannonBase = new PointCollection();
+        private PointCollection _paraArea1 = new PointCollection();
+        private PointCollection _paraArea2 = new PointCollection();
+
+        public BitmapImage _heli01 = new BitmapImage(new Uri("heli01.png", UriKind.Relative));
+        public BitmapImage _heli02 = new BitmapImage(new Uri("heli02.png", UriKind.Relative));
+
+        public BitmapImage ObjectBitmap;
+        private Random rand = new Random();
+
+        public bool ReleaseHit = false;
+
+        //heli
+        public Target(double direction, double formW, double formH)
+        {
+            _direction = direction;
+            _speed = 5;
+            _description = gameObject.Heli;
+
+            if (_direction > 0)
+            {
+                _vertices.Add(new Point(-heliWidth, heliHeight));
+                _vertices.Add(new Point(0, heliHeight));
+                _vertices.Add(new Point(0, 2 * heliHeight));
+                _vertices.Add(new Point(-heliWidth, 2 * heliHeight));
+                ObjectBitmap = _heli01.Clone();
+            }
+            else
+            {
+                _vertices.Add(new Point(formW, heliHeight));
+                _vertices.Add(new Point(formW + heliWidth, heliHeight));
+                _vertices.Add(new Point(formW + heliWidth, 2 * heliHeight));
+                _vertices.Add(new Point(formW, 2 * heliHeight));
+                ObjectBitmap = _heli02.Clone();
+            }
+
+            _battleField.Add(new Point(-5, -5));
+            _battleField.Add(new Point(formW + 5, -5));
+            _battleField.Add(new Point(formW + 5, formH + 5));
+            _battleField.Add(new Point(-5, formH + 5));
+
+            _paraArea1.Add(new Point(heliWidth, heliHeight));
+            _paraArea1.Add(new Point(formW / 2 - 2 * heliWidth, heliHeight));
+            _paraArea1.Add(new Point(formW / 2 - 2 * heliWidth, 2 * heliHeight));
+            _paraArea1.Add(new Point(heliWidth, 2 * heliHeight));
+
+            _paraArea2.Add(new Point(formW - heliWidth, heliHeight));
+            _paraArea2.Add(new Point(formW / 2 + 2 * heliWidth, heliHeight));
+            _paraArea2.Add(new Point(formW / 2 + 2 * heliWidth, 2 * heliHeight));
+            _paraArea2.Add(new Point(formW - heliWidth, 2 * heliHeight));
+        }
+        
+        //paratrooper
+        public Target(PointCollection heli, double formW, double formH)
+        {
+            _direction = Math.PI;
+            _speed = 2;
+            _description = gameObject.Para;
+
+            _vertices.Add(new Point(heli[0].X + heliWidth / 2, 2 * heliHeight));
+            _vertices.Add(new Point(heli[0].X + heliWidth / 2 + paraWidth, 2 * heliHeight));
+            _vertices.Add(new Point(heli[0].X + heliWidth / 2 + paraWidth, 2 * heliHeight + paraHeight));
+            _vertices.Add(new Point(heli[0].X + heliWidth / 2, 2 * heliHeight + paraHeight));
+
+            _ground.Add(new Point(-5, formH - 42));
+            _ground.Add(new Point(formW + 5, formH - 42));
+            _ground.Add(new Point(formW + 5, formH));
+            _ground.Add(new Point(-5, formH));
+
+            _cannonBase.Add(new Point(formW / 2 - 30, formH));
+            _cannonBase.Add(new Point(formW / 2 + 30, formH));
+            _cannonBase.Add(new Point(formW / 2 + 30, formH / 2));
+            _cannonBase.Add(new Point(formW / 2 - 30, formH / 2));
+        }
+        
+        //gift
+        public Target(double formW, double formH)
+        {
+            int casual = rand.Next(1, 6);
+
+            _direction = Math.PI;
+            _speed = 2;
+            _description = gameObject.Gift;
+
+            _vertices.Add(new Point(formW / 6 * casual, -10));
+            _vertices.Add(new Point(formW / 6 * casual + 10, -10));
+            _vertices.Add(new Point(formW / 6 * casual + 10, 0));
+            _vertices.Add(new Point(formW / 6 * casual, 0));
+
+            _ground.Add(new Point(-5, formH - 42));
+            _ground.Add(new Point(formW + 5, formH - 42));
+            _ground.Add(new Point(formW + 5, formH));
+            _ground.Add(new Point(-5, formH));
+        }
+
+        public void UpdatePosition()
+        {
+            if (_description == gameObject.Heli)
+            {
+                for (int i = 0; i < _vertices.Count; i++)
+                {
+                    _vertices[i] = new Point(_vertices[i].X + _speed * Math.Sin(_direction), _vertices[i].Y - _speed * Math.Cos(_direction));
+                }
+
+                if (!IntersectObject(_vertices, _battleField)) _lastFrame = true;
+
+                ReleaseHit = IntersectObject(_vertices, _paraArea1) || IntersectObject(_vertices, _paraArea2);
+            }
+            else if (_description == gameObject.Para)
+            {
+                _speed = _speed * 1.01;
+                for (int i = 0; i < _vertices.Count; i++)
+                {
+                    _vertices[i] = new Point(_vertices[i].X + _speed * Math.Sin(_direction), _vertices[i].Y - _speed * Math.Cos(_direction));
+                }
+
+                if (IntersectObject(_vertices, _ground))
+                {
+                    if (paraHeight == 30)
+                    {
+                        paraHeight = 40;
+                        _vertices[0] = new Point(_vertices[0].X, _vertices[0].Y + 20);
+                        _vertices[1] = new Point(_vertices[1].X, _vertices[1].Y + 20);
+                    }
+                    _direction = -Math.Sign(_vertices[1].X - _ground[1].X / 2) * Math.PI / 2;
+                    _speed = 2;
+                }
+
+                if (IntersectObject(_vertices, _cannonBase))
+                {
+                    _lastFrame = true;
+                    ReleaseHit = true;
+                }
+
+                if (_vertices[0].Y > _cannonBase[0].Y / 4 * 3 && paraHeight == 20)
+                {
+                    _speed = 2;
+                    _vertices[0] = new Point(_vertices[0].X, _vertices[0].Y - paraHeight);
+                    _vertices[1] = new Point(_vertices[1].X, _vertices[1].Y - paraHeight);
+                    paraHeight = 30;
+                }
+            }
+            else if (_description == gameObject.Gift)
+            {
+                for (int i = 0; i < _vertices.Count; i++)
+                {
+                    _vertices[i] = new Point(_vertices[i].X + _speed * Math.Sin(_direction), _vertices[i].Y - _speed * Math.Cos(_direction));
+                }
+
+                if (IntersectObject(_vertices, _ground)) _lastFrame = true;
+            }
+        }
+    }
+}
